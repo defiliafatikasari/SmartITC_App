03831bccec36bf1bf640a690b16eb1d59bb605f4
unknown <defiliafatikasari09@gmail.com>
2024-05-02 02:58:22 +0700

    Perubahan Tampilan LayarKontak

--- src/screen/LayarKontak.jsx
+++ src/screen/LayarKontak.jsx
@@ -9,37 +9,79 @@ const LayarKontak = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Kontak Kami</Text>
-      <View style={styles.contactInfo}>
-      <IkonF name="map-marked-alt" color={biru} size={26} /><Text style={styles.label}>Alamat:</Text>
-        <Text style={{color: hitam}}>Jalan Jalmak Gang.VII, Pamekasan, Jawa Timur</Text>
-      </View>
-      <View style={styles.contactInfo}>
-      <IkonMc name="whatsapp" color={biru} size={26} /><Text style={styles.label}>WhatsApp:</Text>
-        <Text style={{color: hitam}}>+6292197569330</Text>
-      </View>
-      <View style={styles.contactInfo}>
-      <IkonMc name="phone" color={biru} size={26} /><Text style={styles.label}>Phone:</Text>
-        <Text style={{color: hitam}}>+6297862771867</Text>
-      </View>
-      <View style={styles.contactInfo}>
-      <IkonMc name="email" color={biru} size={26} /><Text style={styles.label}>Email:</Text>
-        <Text style={{color: hitam}}>smartITC09@gmail.com</Text>
-      </View>
-      <View style={styles.contactInfo}>
-      <IkonMc name="instagram" color={biru} size={26} /><Text style={styles.label}>Instagram:</Text>
-        <Text style={{color: hitam}}>@smartitc</Text>
-      </View>
-      <View style={styles.contactInfo}>
-      <IkonMc name="facebook" color={biru} size={26} /><Text style={styles.label}>Facebook:</Text>
-        <Text style={{color: hitam}}>Smart ITC</Text>
-      </View>
-      <View style={styles.contactInfo}>
-      <IkonMc name="youtube" color={biru} size={26} /><Text style={styles.label}>YouTube:</Text>
-        <Text style={{color: hitam}}>@smartitc</Text>
-      </View>
-      <View style={styles.contactInfo}>
-      <IkonM name="tiktok" color={biru} size={26} /><Text style={styles.label}>TikTok</Text>
-        <Text style={{color: hitam}}>@smartitc</Text>
+      <View style={styles.contactContainer}>
+        <View style={styles.contactInfo}>
+          <View style={styles.iconContainer}>
+            <IkonF name="map-marked-alt" color={biru} size={20} />
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.label}>Alamat:</Text>
+            <Text style={styles.text}>Jalan Jalmak Gang.VII, Pamekasan, Jawa Timur</Text>
+          </View>
+        </View>
+        <View style={styles.contactInfo}>
+          <View style={styles.iconContainer}>
+            <IkonMc name="whatsapp" color={biru} size={20} />
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.label}>WhatsApp:</Text>
+            <Text style={styles.text}>+6292197569330</Text>
+          </View>
+        </View>
+        <View style={styles.contactInfo}>
+          <View style={styles.iconContainer}>
+            <IkonMc name="phone" color={biru} size={20} />
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.label}>Phone:</Text>
+            <Text style={styles.text}>+6297862771867</Text>
+          </View>
+        </View>
+        <View style={styles.contactInfo}>
+          <View style={styles.iconContainer}>
+            <IkonMc name="email" color={biru} size={20} />
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.label}>Email:</Text>
+            <Text style={styles.text}>smartITC09@gmail.com</Text>
+          </View>
+        </View>
+        <View style={styles.contactInfo}>
+          <View style={styles.iconContainer}>
+            <IkonMc name="instagram" color={biru} size={20} />
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.label}>Instagram:</Text>
+            <Text style={styles.text}>@smartitc</Text>
+          </View>
+        </View>
+        <View style={styles.contactInfo}>
+          <View style={styles.iconContainer}>
+            <IkonMc name="facebook" color={biru} size={20} />
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.label}>Facebook:</Text>
+            <Text style={styles.text}>Smart ITC</Text>
+          </View>
+        </View>
+        <View style={styles.contactInfo}>
+          <View style={styles.iconContainer}>
+            <IkonMc name="youtube" color={biru} size={20} />
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.label}>YouTube:</Text>
+            <Text style={styles.text}>@smartitc</Text>
+          </View>
+        </View>
+        <View style={styles.contactInfo}>
+          <View style={styles.iconContainer}>
+            <IkonM name="tiktok" color={biru} size={20} />
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.label}>TikTok:</Text>
+            <Text style={styles.text}>@smartitc</Text>
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -47,22 +89,45 @@ const LayarKontak = () => {
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 10,
-    paddingLeft: 15,
+    flex: 1,
+    backgroundColor: '#FFF',
+    paddingHorizontal: 20,
+    paddingTop: 20,
   },
   title: {
-    fontSize: 24,
+    fontSize: 28,
     fontWeight: 'bold',
-    color: hitam,
-    marginBottom: 30,
+    color: '#333',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  contactContainer: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 8,
+    
   },
   contactInfo: {
-    color: hitam,
-    marginBottom: 10,
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  iconContainer: {
+    width: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textContainer: {
+    flex: 1,
+    marginLeft: 10,
   },
   label: {
     fontWeight: 'bold',
-    color: hitam,
+    color: '#555',
+    fontSize: 18,
+  },
+  text: {
+    color: '#666',
+    fontSize: 16,
   },
 });
 
